62ccf93df428b162e023d124883b9f1a9060d1ca
Beat Gebistorf <beatgeb@gmail.com>
2019-06-19 12:16:08 +0200

    Fixes #33: Allows to skip empty htl mock entry

--- srv/service.js
+++ srv/service.js
@@ -75,7 +75,7 @@ module.exports = function (pathToComponents, pathToPages, pathToAemMocks, backen
             cmsTemplate = await service.getHtlTemplate(path, view)
             if (cmsTemplate) {
               const engine = require('./htl/engine')
-              raw = await engine(vm.htl, cmsTemplate, {useDir: pathToAemMocks, useOptions: {model: viewModel}})
+              raw = await engine(vm.htl || {}, cmsTemplate, {useDir: pathToAemMocks, useOptions: {model: viewModel}})
               if (raw) {
                 raw = raw.body
               }
