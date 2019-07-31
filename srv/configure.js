0393450d365d1c59bce5e757d5ef2308c53a6e9e
Beat Gebistorf <beatgeb@gmail.com>
2019-07-31 19:45:28 +0200

    Fixes #35: Optimise watcher behaviour

--- srv/configure.js
+++ srv/configure.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const config = require('./config.json')
 const express = require('express')
 const builder = require('./builder')
+const chokidar = require('chokidar')
 const args = minimist(process.argv)
 const { defineRootPath, definePath } = require('./path')
 const rootPath = defineRootPath(args.embedded)
@@ -20,7 +21,54 @@ if (backendTemplates === 'htl') {
   aemMocksPath = definePath(args.aemMocksPath, rootPath, config.aemMocks)
 }
 
+// Watchers
+
+const componentsWatcher = chokidar.watch(componentsPath)
+const pagesWatcher = chokidar.watch(pagesPath)
+const directivesWatcher = chokidar.watch(directivePath)
+const assetsWatcher = chokidar.watch(assetsPath)
+const stylesWatcher = chokidar.watch(stylePath)
+
+componentsWatcher.on('ready', function () {
+  componentsWatcher.on('all', function () {
+    console.log('rebuild components')
+    builder.build(componentsPath, stylePath, scriptPath, path.resolve(__dirname, config.componentsImportFile))
+  })
+})
+
+pagesWatcher.on('ready', function () {
+  pagesWatcher.on('all', function () {
+    console.log('rebuild pages')
+    builder.buildPages(pagesPath, stylePath, scriptPath, path.resolve(__dirname, config.pagesImportFile))
+  })
+})
+
+directivesWatcher.on('ready', function () {
+  directivesWatcher.on('all', function () {
+    console.log('rebuild directives')
+    builder.buildDirectives(directivePath, path.resolve(__dirname, config.directivesImportFile))
+  })
+})
+
+stylesWatcher.on('ready', function () {
+  stylesWatcher.on('all', function () {
+    console.log('get new styles')
+    builder.build(componentsPath, stylePath, scriptPath, path.resolve(__dirname, config.componentsImportFile))
+    builder.buildPages(pagesPath, stylePath, scriptPath, path.resolve(__dirname, config.pagesImportFile))
+  })
+})
+
+assetsWatcher.on('ready', function () {
+  assetsWatcher.on('all', function () {
+    console.log('get new assets')
+    builder.build(componentsPath, stylePath, scriptPath, path.resolve(__dirname, config.componentsImportFile))
+    builder.buildPages(pagesPath, stylePath, scriptPath, path.resolve(__dirname, config.pagesImportFile))
+  })
+})
+
 const api = require('./api')(componentsPath, pagesPath, aemMocksPath, backendTemplates)
+
+// Initial builder
 
 console.log('build index for Vue components', componentsPath)
 console.log('build index for Vue pages', pagesPath)
