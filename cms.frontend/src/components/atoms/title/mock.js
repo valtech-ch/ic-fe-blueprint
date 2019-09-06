de11213f9a0683e335a5503e26df95cdd337a307
Beat Gebistorf <beatgeb@gmail.com>
2019-09-06 15:52:18 +0200

    Update adobe htl engine package

--- cms.frontend/src/components/atoms/title/mock.js
+++ cms.frontend/src/components/atoms/title/mock.js
@@ -2,6 +2,9 @@ module.exports = {
   routes: [],
   models: {
     default: {
+      htl: {
+        text: 'Hello World'
+      },
       text: 'Hello World'
     },
     baum: {
--- srv/service.js
+++ srv/service.js
@@ -96,9 +96,13 @@ module.exports = function (pathToComponents, pathToPages, pathToAemMocks, backen
             cmsTemplate = await service.getHtlTemplate(path, view)
             if (cmsTemplate) {
               const engine = require('./htl/engine')
-              raw = await engine(vm.htl || {}, cmsTemplate, { useDir: pathToAemMocks, useOptions: { model: viewModel } })
-              if (raw) {
-                raw = raw.body
+              try {
+                raw = await engine(vm.htl || {}, cmsTemplate, {
+                  useDir: pathToAemMocks,
+                  useOptions: { model: viewModel }
+                })
+              } catch (e) {
+                errors.push({ text: e.toString() })
               }
 
               if (!raw) {
