b0545ba728327661c2b46df361b5a4718869e515
Thomas Jaggi <thomas@responsive.ch>
2019-09-10 15:44:25 +0200

    fix indentation

--- src/components/ComponentView.vue
+++ src/components/ComponentView.vue
@@ -243,10 +243,10 @@ export default {
 
 .is-fullscreen-demo .tabs-content {
   position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #fff;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #fff;
 }
 </style>
