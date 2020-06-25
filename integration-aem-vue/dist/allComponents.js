614e5c2d5b2fcb77e8fb365bb4bf2ed890176388
Samuel Gomez <samuel.gomez@valtech.com>
2020-06-25 14:04:03 +0200

    Revert "Move everthing into 'integration-aem-vue', as preparation to merge into develop"
    This reverts commit ae2f04bf159b75873c44ea49646c368b8334fd52.
    
    Reverting as this change breaks a reference used by the actual JP project, and we are still figuring out how to update reliably.

--- integration-aem-vue/dist/allComponents.js
+++
@@ -1,7 +0,0 @@
-// created: Sat May 16 2020 23:10:25 GMT+0200 (DST)
-import Vue from 'vue'
-import Icon from '/mnt/c/win-repos/valtech-ch/ic-fe-blueprint/cms.frontend/src/components/atoms/icon/Icon.vue'
-import HelloWorld from '/mnt/c/win-repos/valtech-ch/ic-fe-blueprint/cms.frontend/src/components/molecules/HelloWorld/HelloWorld.vue'
-
-Vue.component('Icon', Icon)
-Vue.component('HelloWorld', HelloWorld)
--- integration-aem-vue/dist/allPages.js
+++
@@ -1,5 +0,0 @@
-// created: Sat May 16 2020 23:10:25 GMT+0200 (DST)
-import Vue from 'vue'
-import Demo from '/mnt/c/win-repos/valtech-ch/ic-fe-blueprint/cms.frontend/src/pages/Demo.vue'
-
-Vue.component('Demo', Demo)
