73a6b33783e3f9cfebbb44ee6e37a0baa5653c6d
philippvlasov <pylyp.vlasov@valtech.com>
2020-11-18 16:05:11 +0200

    Add nested css selectors for postcss

--- integration-sitecore-react/postcss.config.js
+++ integration-sitecore-react/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-import-ext-glob'),
+    require('postcss-nested'),
     require('tailwindcss'),
     require('autoprefixer')
   ]
